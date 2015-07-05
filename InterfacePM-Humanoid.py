38a00f525c07830e93d8beded7bda53c0174053e
SL-prog <supp0rt.goog1e@gmail.com>
2015-07-05 13:20:59 +0200

    Ajout icone, modification background, et test des sliders
    Ne fonctionne pas (sliders varient en même temps).

--- InterfacePM-Humanoid.py
+++ InterfacePM-Humanoid.py
@@ -1,66 +1,59 @@
 ﻿import serial
 from tkinter import *
 
+Servo_tete_x = 90
+Servo_tete_y = 90
+
 class Window(Frame):
     def __init__(self, master, **kwargs):
         Frame.__init__(self, master, width=600, height=400, **kwargs)
         self.master = master
 
         self.photo = PhotoImage(file="pm.gif")
-        self.canvas = Canvas(self,width=550, height=555)
-        self.canvas.create_image(275,277, image=self.photo)
+        self.canvas = Canvas(self,width=550, height=588)
+        self.canvas.create_image(275,294, image=self.photo)
         self.canvas.pack()
 
-        self.terminal = Canvas(self, width=350, height=555, bg='black')
+        self.terminal = Canvas(self, width=350, height=588, bg='black')
         self.terminal.pack()
-        self.terminal.create_text(50,10,text='Servo_tete_x : 80',fill="white")
-        self.terminal.create_text(50,30,text='Servo_tete_y : 63',fill="white")
-
-        self.terminal.create_text(45,545,text='Baud : 9600',fill="white")
-        self.terminal.create_text(305,545,text='Port : COM8',fill="white")
 
         self.canvas.grid(column=1,row=1)
-
         self.terminal.grid(column=2,row=1)
 
+        self.A = Scale(master, from_=0, to=180, orient=VERTICAL, length=80, showvalue=0, variable = Servo_tete_x, command=self.envoi)
+        self.A.pack()
+        self.A_window = self.canvas.create_window(232, 18, anchor=NW, window=self.A)
 
-        self.test = Scale(master, from_=90, to=180, orient=HORIZONTAL)
-        self.test.pack()
-        self.test_window = self.canvas.create_window(400, 10, anchor=NW, window=self.test)
+        self.B = Scale(master, from_=0, to=180, orient=HORIZONTAL, length=80, showvalue=0, variable = Servo_tete_y, command=self.envoi)
+        self.B.pack()
+        self.B_window = self.canvas.create_window(232, 140, anchor=NW, window=self.B)
 
-        self.test2 = Scale(master, from_=0, to=180, orient=VERTICAL)
-        self.test2.pack()
-        self.test2_window = self.canvas.create_window(500, 10, anchor=NW, window=self.test2)
 
-        self.tetex1 = Button(self, text="+", command=self.msg )
-        self.tetex1.pack(side="bottom", fill=BOTH)
-        self.tetex1_window = self.canvas.create_window(236, 48, anchor=NW, window=self.tetex1)
-        self.tetex2 = Button(self, text="-", command=self.msg )
-        self.tetex2.pack(side="bottom", fill=BOTH)
-        self.tetex2_window = self.canvas.create_window(236, 75, anchor=NW, window=self.tetex2)
-
-        self.tetey1 = Button(self, text="+", command=self.msg )
-        self.tetey1.pack(side="bottom", fill=BOTH)
-        self.tetey1_window = self.canvas.create_window(295, 85, anchor=NW, window=self.tetey1)
-        self.tetey2 = Button(self, text="-", command=self.msg )
-        self.tetey2.pack(side="bottom", fill=BOTH)
-        self.tetey2_window = self.canvas.create_window(315, 85, anchor=NW, window=self.tetey2)
-
-        self.LED_state = True
+        #self.LED_state = True
         #self.serial = serial.Serial(port="COM8")
 
-    def msg(self):
-        if self.LED_state:
-            self.serial.write(bytes('a', encoding="utf-8"))
-        else:
-            self.serial.write(bytes('z', encoding="utf-8"))
-        self.LED_state = not self.LED_state
+        self.envoi()
+
+    def envoi(self):
+        #if self.LED_state:
+            #self.serial.write(bytes('a', encoding="utf-8"))
+        #else:
+            #self.serial.write(bytes('z', encoding="utf-8"))
+        #self.LED_state = not self.LED_state
+
+        self.terminal.create_text(60,10,text="A = Servo_tete_x : "+str(Servo_tete_x),fill="white")
+        self.terminal.create_text(60,30,text="B = Servo_tete_y : "+str(Servo_tete_y),fill="white")
+
+        self.terminal.create_text(45,573,text='Baud : 9600',fill="white")
+        self.terminal.create_text(305,573,text='Port : COM8',fill="white")
+
 
 if __name__ == '__main__':
     fen = Tk()
+    fen.wm_title("Interface PM Humanoid")
+    fen.iconbitmap(default='pm.ico')
 
     w = Window(fen)
     w.pack()
-
 
     fen.mainloop()
